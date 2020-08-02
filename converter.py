7f9799e7be1d6a83e039e55c576fa91185040382
lux <lux.sol@outlook.com>
2020-08-02 05:29:29 -0400

    Add function to converter for printing collected text to .txt file

--- converter.py
+++ converter.py
@@ -19,7 +19,7 @@ class Converter(object):
     def scrape_text(self, source):
         extndr = Extender(source)
         temp_text = extndr.call_extract()
-        self.text = self.text + str(temp_text) + "\n"
+        self.text = self.text + str(temp_text) #+ "\n"
        
     #Strip string from collected text   
     def strip_string(self, string):
@@ -37,6 +37,25 @@ class Converter(object):
         temp_text = temp_text.replace(temp_text[slice_start:slice_end + 1], "")
         self.text = temp_text
 
+    #Discard collected text that appears after the given string
+    def strip_page(self, string):
+        temp_text = self.text
+        if string in temp_text:
+            slice_start = temp_text.index(string)
+            temp_text = temp_text[:slice_start]
+
+        self.text = temp_text
+
+    #Print collected text to a .txt file
+    def print_text(self, file_name):
+        with open(file_name, "w") as temp_file:
+            for char in self.text:
+                temp_file.write(char)
+                if char == ".":
+                    temp_file.write("\n")
+
+
+
 #Construct object
 converter = Converter()
 
@@ -46,7 +65,7 @@ text = converter.get_text()
 #scrape text to string
 converter.scrape_text("/home/lux/Downloads/test/test3.docx")
 text = converter.get_text()
-print(text)
+#print(text)
 
 #strip word from string
 #converter.strip_string("test")
@@ -54,13 +73,18 @@ print(text)
 #print(text)
 
 #strip slice from string
-converter.strip_slice('"', '"')
-text = converter.get_text()
-print(text)
+#converter.strip_slice('"', '"')
+#text = converter.get_text()
+#print(text)
 
 converter.strip_slice('!!', '!!')
 text = converter.get_text()
 print(text)
 
+#strip page following string
+#converter.strip_page("References")
+#text = converter.get_text()
+#print(text)
 
+converter.print_text("myfile")
 
