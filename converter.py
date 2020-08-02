df47951bb83d138eeddd3368d1e26f5e3769d069
lux <lux.sol@outlook.com>
2020-08-02 00:59:17 -0400

    Add function for stripping characters from collected text

--- converter.py
+++ converter.py
@@ -13,20 +13,37 @@ class Converter(object):
     def get_text(self):
         return self.text
 
-    #Scrape text from source document
+    #Scrape text from source document to string for manipulation
     def scrape_text(self, source):
         temp_text = textract.process(source)
         self.text = self.text + str(temp_text)
-        
+       
+    #Strip characters from collected text   
+    def strip_text(self, word):
+        temp_text = self.text
+        if word in temp_text:
+            temp_text = temp_text.replace(word, "")
+
+        self.text = temp_text
+
 
 #Construct object
 converter = Converter()
 
 #Return collected text
 text = converter.get_text()
-print(text)
 
+#scrape text to string
 converter.scrape_text("/home/lux/Downloads/test/test3.docx")
 text = converter.get_text()
-print("doc3")
 print(text)
+
+#strip word from string
+converter.strip_text("test")
+text = converter.get_text()
+print(text)
+
+converter.strip_text("This is a" )
+text = converter.get_text()
+print(text)
+
