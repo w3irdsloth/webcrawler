bdc9afc585449f70f8db69dddf7e94f543de9f47
lux <lux.sol@outlook.com>
2020-08-03 20:45:36 -0400

    Create a models file to store and extract model commands

--- modeler.py
+++ modeler.py
@@ -1,80 +1,76 @@
+import models
+
 class Modeler(object):
     """ Creates an object for selecting and running a default rnn model """
     def __init__(self):
         self.model = "n/a"
-        self.modlist = ["textgenrnn"]
-        self.implist = []
-        self.fnckey = []
-        self.constructor = "n/a"
-
+        self.mod_list = models.mod_list 
+        self.imp_key = models.imp_key
+        self.fun_key = models.fun_key
+        self.constructors = models.con_key
+ 
     #Get current model
     def get_model(self):
         return self.model
 
     #Get list of available models
     def get_modlist(self):
-        return self.modlist
+        return self.mod_list
 
-    #Get list of necessary imports for selected model
-    def get_imports(self):
-        return self.implist
+    #Get list of necessary imports for a model
+    def get_imports(self, model):
+        return self.imp_key.get(model)
 
     #Get list of available commands for selected model
-    def get_functions(self):
-        return self.fnckey
+    def get_functions(self, model):
+        return self.fun_key.get(model)
 
     #Get constructor for selected model
     def get_constructor(self):
-        return self.constructor
+        return self.constructors
 
     #Set current model
     def set_model(self, model):
-        if  model in self.modlist:
+        if  model in self.mod_list:
             print("Selected " + model + "...")
             self.model = model
 
         else:
             print("Model not found")
 
+    def update_models(self):
+        print("Updating model...")
+        self.mod_list = models.mod_list 
+        self.imp_key = models.imp_key
+        self.fun_key = models.fun_key
+        self.constructors = models.con_key
+
     #Activate current model
     def activate_model(self):
         model = self.model
-        if model == "textgenrnn":
-            self.implist = ["textgenrnn"]
-            from textgenrnn import textgenrnn
-            self.fnckey.append(".train_from_file(source, epochs)")
-            self.constructor = textgenrnn()
-
-        else:
-            print("No active model found")
-
-    #import new model
-    #def import_model(self, model):
-    #    self.modlist.append(model)
-
 
 
 ####Functions####
 
 #Construct model object
-#model = Modeler()
+model = Modeler()
 
 #Get selected model
-#modname=model.get_model()
-#print("Current model: ")
-#print(modname)
+modname=model.get_model()
+print("Current model: ")
+print(modname)
 #print("\n")
 
 #Get list of models
-#modlist = model.get_modlist()
-#print("List of models: ")
-#print(modlist)
+modlist = model.get_modlist()
+print("List of models: ")
+print(modlist)
 #print("\n")
 
 #Set a new model
-#model.set_model("textgenrnn")
-#modelname=model.get_model()
-#print(modelname)
+model.set_model("textgenrnn")
+modelname=model.get_model()
+print(modelname)
 #print("\n")
 
 #Get selected model
@@ -84,15 +80,15 @@ class Modeler(object):
 #print("\n")
 
 #Get list of imports
-#implist = model.get_imports()
-#print("List of imports: ")
-#print(implist)
+implist = model.get_imports("textgenrnn")
+print("List of imports: ")
+print(implist)
 #print("\n")
 
 #Get list of commands
-#cmdlist = model.get_commands()
-#print("List of commands: ")
-#print(cmdlist)
+cmdlist = model.get_functions("textgenrnn")
+print("List of commands: ")
+print(cmdlist)
 #print("\n")
 
 #Get the constructor
---
+++ models.py
@@ -0,0 +1,7 @@
+mod_list = ["textgenrnn"]
+imp_key = {"textgenrnn": ['from textgenrnn import textgenrnn']
+        }
+fun_key = {"textgenrnn": {".train_from_file": ["source", "epochs"]}
+        }
+con_key = {"textgenrnn": "textgenrnn()"}
+
